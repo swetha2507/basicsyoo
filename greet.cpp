7d97a7420080995fd0a63acff4ee7d7beb9bdf56
SWETHA S <swethasrao25@gmail.com>
2021-10-11 11:28:43 +0530

    added code

--- greet.cpp
+++ greet.cpp
@@ -2,3 +2,12 @@
 // Created by SWETHA S on 11/10/21.
 //
 
+#include <iostream>
+using namespace std;
+
+int main(){
+   string name;
+    cout<<"enter your name"<<endl;
+    cin>> name;
+    cout<<"hello, "<< name<<endl;
+}
